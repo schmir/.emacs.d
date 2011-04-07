9add1d4097b228440ce1e77cf81acdc3c9278373
Ralf Schmitt <ralf@systemexit.de>
2011-04-07 10:20:11 +0200

    add cython-mode.el

---
+++ cython-mode.el
@@ -0,0 +1,16 @@
+(require 'python-mode)
+
+;;;###autoload
+(define-derived-mode cython-mode python-mode "Cython"
+  (font-lock-add-keywords
+   nil
+   `((,(concat "\\<\\(NULL"
+               "\\|c\\(def\\|har\\|typedef\\)"
+               "\\|e\\(num\\|xtern\\)"
+               "\\|float"
+               "\\|in\\(clude\\|t\\)"
+               "\\|object\\|public\\|struct\\|type\\|union\\|void"
+               "\\)\\>")
+      1 font-lock-keyword-face t))))
+
+(provide 'cython-mode)
