ba55896ed38a6d109bb7b7c575505d50510b61b5
Ralf Schmitt <ralf@systemexit.de>
2013-01-15 20:37:34 +0100

    add version specific python variants to interpreter-mode-alist

--- init.el
+++ init.el
@@ -671,7 +671,9 @@ completion buffers."
   (add-to-list 'auto-mode-alist
 	       '("\\wscript$\\|\\SConstruct$\\|\\SConscript$\\|\\.py$\\|\\.jy$\\|\\.py\\.cov$" . python-mode))
 
-  (add-to-list 'interpreter-mode-alist '("python" . python-mode))
+  (loop for i in '("jython" "pypy" "python" "python2" "python2.4" "python2.5" "python2.6" "python2.7" "python3" "python3.0" "python3.1" "python3.2" "python3.3")
+	do (add-to-list 'interpreter-mode-alist `(,i . python-mode)))
+
 
   ;; highlight self in python-mode
   (font-lock-add-keywords 'python-mode '(("\\<\\(self\\)" 1 font-lock-builtin-face)))
