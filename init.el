b8345bb9e9afc9d8b270d03c844d7780009a1e11
Ralf Schmitt <ralf@systemexit.de>
2013-03-18 21:48:08 +0100

    install deft

--- init.el
+++ init.el
@@ -38,7 +38,7 @@
 		 whole-line-or-region python-pep8 smex ssh-config-mode
 		 textile-mode yaml-mode elscreen quack auto-complete
 		 escreen smart-tab gist w3m php-mode
-		 pkgbuild-mode markdown-mode textile-mode nrepl)
+		 pkgbuild-mode markdown-mode textile-mode nrepl deft)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
