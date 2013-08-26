98374f71888f7d3653e937162c3cb73635787cca
Ralf Schmitt <ralf@systemexit.de>
2013-08-26 13:53:28 +0200

    nrepl configuration

--- init.el
+++ init.el
@@ -45,6 +45,24 @@
 ;; fix indentation of cond expressions
 (put 'cond 'clojure-backtracking-indent '(2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4))
 
+(setq nrepl-init-code
+"(do
+    (set! *print-length* 150)
+    (set! *print-level* 20)
+    (use 'clojure.pprint)
+    (use 'clojure.reflect))
+")
+
+(defun schmir-setup-nrepl ()
+  (interactive)
+  (insert nrepl-init-code)
+  (nrepl-return))
+(add-hook 'nrepl-connected-hook 'schmir-setup-nrepl)
+
+(setq nrepl-hide-special-buffers t)
+(setq nrepl-popup-stacktraces-in-repl t)
+(setq nrepl-history-file "~/.emacs.d/nrepl-history")
+
 (defun prelude-packages-installed-p ()
   (loop for p in prelude-packages
 	when (not (package-installed-p p)) do (return nil)
