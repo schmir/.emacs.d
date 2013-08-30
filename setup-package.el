dacebaa6b212af98415b724f0df3d7a40622c0dc
Ralf Schmitt <ralf@systemexit.de>
2013-08-30 23:24:54 +0200

    remove some unused packages

--- setup-package.el
+++ setup-package.el
@@ -5,14 +5,12 @@
 	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-(defvar prelude-packages
-  '(clojure-mode clojure-cheatsheet paredit bm boxquote key-chord lua-mode magit git-messenger diminish
-		 highlight-symbol framemove rainbow-mode jedi
-		 whole-line-or-region python-pep8 smex ssh-config-mode
-		 textile-mode yaml-mode elscreen quack auto-complete
-		 escreen smart-tab gist w3m php-mode
-		 pkgbuild-mode markdown-mode textile-mode nrepl deft
-		 rainbow-delimiters shell-pop)
+(defvar prelude-packages '(pkg-info clojure-mode clojure-cheatsheet
+  paredit bm boxquote key-chord lua-mode magit git-messenger diminish
+  highlight-symbol framemove rainbow-mode jedi whole-line-or-region
+  python-pep8 ssh-config-mode textile-mode yaml-mode auto-complete
+  escreen smart-tab gist w3m php-mode markdown-mode textile-mode nrepl
+  deft rainbow-delimiters shell-pop)
   "A list of packages to ensure are installed at launch.")
 
 (defun prelude-packages-installed-p ()
