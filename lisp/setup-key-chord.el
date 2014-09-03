ac0b73a675af6a597e35670ae37795cae6541b5c
Ralf Schmitt <ralf@systemexit.de>
2014-09-03 11:15:45 +0200

    uncomment keychords I don't use anymore

--- lisp/setup-key-chord.el
+++ lisp/setup-key-chord.el
@@ -3,10 +3,10 @@
 
 (key-chord-mode 1)
 
-(key-chord-define-global "yu" 'whole-line-or-region-yank)
-(key-chord-define-global "yy" 'yank-pop)
-(key-chord-define-global "ui"     "\M-w") ;; copy
-(key-chord-define-global "io"     "\C-w") ;; cut
+;; (key-chord-define-global "yu" 'whole-line-or-region-yank)
+;; (key-chord-define-global "yy" 'yank-pop)
+;; (key-chord-define-global "ui"     "\M-w") ;; copy
+;; (key-chord-define-global "io"     "\C-w") ;; cut
 
 (key-chord-define-global "nm"     'ido-switch-buffer)
 
@@ -17,8 +17,8 @@
 (key-chord-define-global ";\""    'comment-dwim)
 (key-chord-define-global ";'"    'comment-dwim)
 
-(key-chord-define-global "hj"     'toggle-windows-split)
-(key-chord-define-global "jk"     'flip-window)
-(key-chord-define-global "kl"     'other-frame)
+;; (key-chord-define-global "hj"     'toggle-windows-split)
+;; (key-chord-define-global "jk"     'flip-window)
+;; (key-chord-define-global "kl"     'other-frame)
 
 (provide 'setup-key-chord)
