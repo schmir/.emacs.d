eb94ecc3f05403807aee7f690bed2b7a62cfa69f
Ralf Schmitt <ralf@systemexit.de>
2014-10-10 15:20:56 +0200

    disable mouse-avoidance-mode

--- lisp/setup-mouse.el
+++ lisp/setup-mouse.el
@@ -1,6 +1,12 @@
 (mouse-wheel-mode 1)
 ;; Drive out the mouse when it's too near to the cursor.
-(mouse-avoidance-mode 'exile)
+
+
+;; exile makes my mouse jump when *selecting* text with the mouse
+
+;; (mouse-avoidance-mode 'exile)
+(mouse-avoidance-mode 'none)
+
 (setq mouse-avoidance-threshold 10
       mouse-avoidance-nudge-dist 20
       mouse-avoidance-nudge-var 5)
