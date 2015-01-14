276e3ec336a4227bba7c5ab97217d9a9d68754ea
Ralf Schmitt <ralf@systemexit.de>
2015-01-14 22:04:13 +0100

    fix the "buffer scratch has no process error"
    see https://stackoverflow.com/questions/3704962/how-to-know-if-emacs-server-is-running

--- lisp/setup-server.el
+++ lisp/setup-server.el
@@ -1,10 +1,16 @@
-(unless (= 0 (user-uid))
-  ;; Only start server mode if it isn't started already
-  (when (or (not (boundp 'server-process))
-	    (not (eq (process-status server-process)
-		     'listen)))
-    (server-start)))
 
+(defun running-as-server-p ()
+  "Returns true if `server-start' has been called."
+  (condition-case nil
+      (and (boundp 'server-process)
+	   (memq (process-status server-process)
+		 '(connect listen open run)))
+    (error)))
+
+;; Only start server mode if it isn't started already
+(unless (= 0 (user-uid))
+  (when (not (running-as-server-p))
+    (server-start)))
 
 (add-hook 'server-done-hook 'delete-frame)
 
