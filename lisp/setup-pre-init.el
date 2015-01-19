70444fe7ca03bfbf266bd9acf79c549c01caa711
Ralf Schmitt <ralf@systemexit.de>
2015-01-19 22:58:38 +0100

    cleanup setup-pre-init a bit

--- lisp/setup-pre-init.el
+++ lisp/setup-pre-init.el
@@ -3,15 +3,10 @@
 (setq my-private-file "~/.private.el")
 ;; (setq generated-autoload-file (concat dotfiles-dir "loaddefs.el"))
 (setq home-dir (getenv "HOME"))
-(setq custom-theme-directory (concat dotfiles-dir "themes/"))
-
 (defun add-load-path (path)
   (add-to-list 'load-path path))
 
 (add-load-path vendor-dir)
-(add-load-path (concat vendor-dir "emacs-w3m"))
-(add-load-path (concat vendor-dir "bbdb"))
-(add-load-path (concat vendor-dir "auto-complete"))
 
 (if (file-exists-p custom-file)
     (load custom-file))
