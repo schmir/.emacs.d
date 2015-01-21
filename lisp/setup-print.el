b26d584d6bbfa7476f4b365ad201de718b056db8
Ralf Schmitt <ralf@systemexit.de>
2015-01-21 17:12:46 +0100

    fix setup-print

--- lisp/setup-print.el
+++ lisp/setup-print.el
@@ -5,14 +5,15 @@
       ps-n-up-printing 2
       ps-n-up-border-p nil
       ps-line-number t
-      ps-line-number-step 'zebra
+      ps-line-number-step 10
       ps-left-margin 14
       ps-right-margin 14
       ps-top-margin 14
       ps-bottom-margin 14
-      ps-zebra-stripes t
+      ps-zebra-stripes nil
       ps-zebra-stripe-height 10
       ps-zebra-color 0.98
+      ps-print-color-p nil
       ps-paper-type 'a4)
 
 (provide 'setup-print)
