3345d50284e273248d216024b4d8e7ff5c76417a
Ralf Schmitt <ralf@systemexit.de>
2015-02-02 09:46:29 +0100

    shorten setup-package.el

--- lisp/setup-package.el
+++ lisp/setup-package.el
@@ -5,28 +5,24 @@
 ;;	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (package-initialize)
 
-(defvar prelude-packages '(use-package pkg-info slamhound
-  smartparens boxquote diminish
-  framemove rainbow-mode whole-line-or-region
-  textile-mode yaml-mode auto-complete
-  smart-tab gist w3m flycheck
-  nginx-mode batch-mode bbdb)
+(defvar my-package-list
+  '(use-package pkg-info slamhound
+     smartparens boxquote diminish
+     framemove rainbow-mode whole-line-or-region
+     textile-mode yaml-mode auto-complete
+     smart-tab gist w3m flycheck
+     nginx-mode batch-mode bbdb)
   "A list of packages to ensure are installed at launch.")
 
-(defun prelude-packages-installed-p ()
-  (loop for p in prelude-packages
-	when (not (package-installed-p p)) do (return nil)
-	finally (return t)))
+; fetch the list of packages available
+(unless package-archive-contents
+  (message "refreshing package database...")
+  (package-refresh-contents))
 
-(unless (prelude-packages-installed-p)
-  ;; check for new packages (package versions)
-  (message "%s" "Emacs is now refreshing its package database...")
-  (package-refresh-contents)
-  (message "%s" " done.")
-  ;; install the missing packages
-  (dolist (p prelude-packages)
-    (when (not (package-installed-p p))
-      (package-install p))))
+; install the missing packages
+(dolist (package my-package-list)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 (require 'diminish)
 
