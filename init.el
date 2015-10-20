b48aba143d91b14803fb58f2f0014caa71432ae8
Ralf Schmitt <ralf@systemexit.de>
2015-10-20 07:54:22 +0200

    fix indendation of cond expression with latest clojure-mode
    see https://github.com/clojure-emacs/clojure-mode/issues/337

--- init.el
+++ init.el
@@ -185,12 +185,27 @@
 
     (define-clojure-indent
       (event-handler 'defun))
-    ;; (put 'cond-> 'clojure-backtracking-indent '(2 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4))
-    ;; (put-clojure-indent 'cond-> nil)
-
     ;; fix indentation of cond expressions
-    (put 'cond 'clojure-backtracking-indent '(2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4 2 4))
-    (put-clojure-indent 'cond nil)))
+    ;; see https://github.com/clojure-emacs/clojure-mode/issues/337
+    (defun indent-cond (indent-point state)
+      (goto-char (elt state 1))
+      (let ((pos -1)
+	    (base-col (current-column)))
+	(forward-char 1)
+	;; `forward-sexp' will error if indent-point is after
+	;; the last sexp in the current sexp.
+	(condition-case nil
+	    (while (and (<= (point) indent-point)
+			(not (eobp)))
+	      (clojure-forward-logical-sexp 1)
+	      (cl-incf pos))
+	  ;; If indent-point is _after_ the last sexp in the
+	  ;; current sexp, we detect that by catching the
+	  ;; `scan-error'. In that case, we should return the
+	  ;; indentation as if there were an extra sexp at point.
+	  (scan-error (cl-incf pos)))
+	(+ base-col (if (evenp pos) 4 2))))
+    (put-clojure-indent 'cond #'indent-cond)))
 
 (use-package magit :ensure t
   :commands (magit-status)
