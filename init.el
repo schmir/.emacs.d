1c461eb15330741c6dff09b508ff45dccae57885
Ralf Schmitt <ralf@brainbot.com>
2019-06-28 14:26:14 +0200

    Give me back isearch

--- init.el
+++ init.el
@@ -260,7 +260,7 @@
         try-complete-lisp-symbol))
 
 (global-set-key (kbd "<C-tab>") 'hippie-expand)
-(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "C-S-s") 'swiper-isearch)
 (global-set-key (kbd "C-c s") 'magit-status)
 (global-set-key (kbd "S-SPC") (lambda() (interactive) (cycle-spacing -1)))
 (require 'setup-server)
