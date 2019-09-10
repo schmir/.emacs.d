d59e46a376c362e30b284b79a9d2497ede9ca480
Ralf Schmitt <ralf@systemexit.de>
2019-09-10 15:46:57 +0200

    Setup tide for typescript

--- init.el
+++ init.el
@@ -98,6 +98,7 @@
    solidity-mode
    spacemacs-theme
    swiper
+   tide
    which-key
    yaml-mode
    zenburn-theme))
@@ -268,6 +269,25 @@
   (add-hook 'racer-mode-hook #'company-mode)
   (add-hook 'rust-mode-hook #'flycheck-mode)
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
+
+
+;; --- setup typescript
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+;; aligns annotation to the right hand side
+;; (setq company-tooltip-align-annotations t)
+
+;; formats the buffer before saving
+;; (add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 
 (require 'lsp-java)
