ea3357c42404690b3dbb5d9f900d1efdab0247bd
Ralf Schmitt <ralf@systemexit.de>
2019-11-10 21:51:11 +0100

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" default))
+   '("85d1dbf2fc0e5d30f236712b831fb24faf6052f3114964fdeadede8e1b329832" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" default))
  '(package-selected-packages
-   '(hippie-expand use-package zenburn-theme git-messenger leuven-theme persistent-scratch highlight-symbol dockerfile-mode which-key aggressive-indent leuven-themes spacemacs-themes spacemacs-theme smartparens yaml-mode smex markdown-mode magit htmlize elpy counsel-projectile cider boxquote))
+   '(writegood-mode terraform-mode hcl-mode hippie-expand use-package zenburn-theme git-messenger leuven-theme persistent-scratch highlight-symbol dockerfile-mode which-key aggressive-indent leuven-themes spacemacs-themes spacemacs-theme smartparens yaml-mode smex markdown-mode magit htmlize elpy counsel-projectile cider boxquote))
  '(shell-pop-shell-type '("eshell" "*eshell*" (lambda nil (eshell)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
