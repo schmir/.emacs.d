e05b7c651a62bd037c9fa5cc2e7235ba9eaf8b6a
Ralf Schmitt <ralf@systemexit.de>
2020-01-20 15:37:43 +0100

    Update custom.el

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("85d1dbf2fc0e5d30f236712b831fb24faf6052f3114964fdeadede8e1b329832" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" default))
+   '("672bb062b9c92e62d7c370897b131729c3f7fd8e8de71fc00d70c5081c80048c" "6096a2f93610f29bf0f6fe34307587edd21edec95073cbfcfb9d7a3b9206b399" "85d1dbf2fc0e5d30f236712b831fb24faf6052f3114964fdeadede8e1b329832" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" "170bb47b35baa3d2439f0fd26b49f4278e9a8decf611aa33a0dad1397620ddc3" default))
  '(package-selected-packages
-   '(writegood-mode terraform-mode hcl-mode hippie-expand use-package zenburn-theme git-messenger leuven-theme persistent-scratch highlight-symbol dockerfile-mode which-key aggressive-indent leuven-themes spacemacs-themes spacemacs-theme smartparens yaml-mode smex markdown-mode magit htmlize elpy counsel-projectile cider boxquote))
+   '(impatient-mode flymd lispy writegood-mode terraform-mode hcl-mode hippie-expand use-package zenburn-theme git-messenger leuven-theme persistent-scratch highlight-symbol dockerfile-mode which-key aggressive-indent leuven-themes spacemacs-themes spacemacs-theme smartparens yaml-mode smex markdown-mode magit htmlize elpy counsel-projectile cider boxquote))
  '(shell-pop-shell-type '("eshell" "*eshell*" (lambda nil (eshell)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
