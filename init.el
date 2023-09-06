31716880fed8dc6df39bdd2c588596dd5673fda9
Ralf Schmitt <ralf@systemexit.de>
2023-09-06 03:00:51 +0200

    Only redirect eln cache when using native comp

--- init.el
+++ init.el
@@ -21,7 +21,8 @@
   :demand t
   :config
   (progn
-    (when (fboundp 'startup-redirect-eln-cache)
+    (when (and (native-comp-available-p)
+               (fboundp 'startup-redirect-eln-cache))
       (startup-redirect-eln-cache
        (convert-standard-filename
         (expand-file-name  "var/eln-cache/" user-emacs-directory))))
