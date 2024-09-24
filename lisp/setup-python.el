f754043049f323cfe028be2ccce269a931026a09
Ralf Schmitt <ralf@systemexit.de>
2024-09-24 18:17:11 +0200

    Use fm-ruff instead of flymake-ruff

--- lisp/setup-python.el
+++ lisp/setup-python.el
@@ -10,11 +10,10 @@
 (setup (:package python-pytest))
 
 (setup python
-  (:package flymake-ruff)
   (:also-load python-pytest)
   (setopt python-shell-interpreter "python3")
   (:with-mode (python-mode python-ts-mode)
-    (:hook #'flymake-ruff-load #'my/setup-eglot-flymake-backend #'flymake-mode #'eglot-ensure))
+    (:hook #'fm-ruff-setup #'my/setup-eglot-flymake-backend #'flymake-mode #'eglot-ensure))
   (when (treesit-ready-p 'python)
     (add-to-list 'major-mode-remap-alist '(python-mode . python-ts-mode)))
   (advice-add 'run-python :around #'with-project-root-as-default-directory))
